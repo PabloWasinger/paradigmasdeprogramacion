b21584e9fe6460d0f09b5f1620a10411d1bc4bcd
pablowasinger <pablowasinger@protonmail.com>
2024-03-18 15:24:37 -0300

    cambios a vessel.hs

--- Tp1/vessel.hs
+++ Tp1/vessel.hs
@@ -15,7 +15,8 @@ listaStack n1 n2 =  n1 == []
         otherwise newS n2 : listaStack (n1-1)
 
 newV :: Int -> Int -> Route -> Vessel  -- construye un barco según una cantida de bahias, la altura de las mismas y una ruta
-newV i1 i2 r = Ves (listaStack i1 i2) r
+newV cantidad altura ruta|cantidad == 0 = []
+                         |otherwise = Ves [Stack.newS altura] ++ newV (cantidad - 1) altura ruta
 
 
 freeCellsV :: Vessel -> Int            -- responde la celdas disponibles en el barco
@@ -27,4 +28,4 @@ unloadV :: Vessel -> String -> Vessel  -- responde un barco al que se le han des
 unloadV (Ves st i) s = (Ves [popS y s|y <- st] i)
 
 netV :: Vessel -> Int                  -- responde el peso neto en toneladas de los contenedores en el barco
-netV (Ves st i) = (Ves [netS y|y <- st] i)
+netV (Ves st _) = sum (map netS st)
