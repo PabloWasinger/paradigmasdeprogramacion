48266d1e606274a26aec7bdf3fc20ec56b01e9f8
pablowasinger <pablowasinger@protonmail.com>
2024-03-17 23:37:59 -0300

    asd

--- Tp1/route.hs
+++ Tp1/route.hs
@@ -7,8 +7,8 @@ import Data.List
 data Route = Rou [ String ] deriving (Eq, Show)
 
 newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
-newR xs = (Rou xs)
+newR = Rou 
 
 
 inOrderR :: Route -> String -> String -> Bool  -- indica si la primer ciudad consultada esta antes que la segunda ciudad en la ruta
-inOrderR (Rou xs) c1 c2 = (elemIndex c1 xs) < (elemIndex c2 xs)
+inOrderR (Rou xs) c1 c2 = elemIndex c1 xs < elemIndex c2 xs
--- Tp1/stack.hs
+++ Tp1/stack.hs
@@ -1,17 +1,53 @@
-module Stack ( Stack, newS, freeCellsS )
+module Stack ( Stack,newS, freeCellsS, stackS)
  where
 
-import Container
-import Route
-import Data.ByteString (length)
+--import Route
+--import Container
 
+
+data Container = Con String Int deriving (Eq, Show)
 data Stack = Sta [ Container ] Int deriving (Eq, Show)
+data Route = Rou [ String ] deriving (Eq, Show)
+
+
 
 newS :: Int -> Stack                          -- construye una Pila con la capacidad indicada 
-newS x = (Sta [] x) 
+newS x = Sta [] x
 
 freeCellsS :: Stack -> Int                    -- responde la celdas disponibles en la pila
 freeCellsS (Sta xs x) = x - length xs 
 
 stackS :: Stack -> Container -> Stack         -- apila el contenedor indicado en la pila
-stackS (Sta x n) (Con s i) = (Sta [x ++ [(Con s i)]] n)
+stackS (Sta xs n) (Con s i) = Sta (xs ++ [Con s i]) n
+
+
+listaP :: [Container] -> [Int]                -- Recibe una lista de containers y devuelve una lista con los pesos de cada container
+listaP xs = [peso y|y<-xs]
+
+peso :: Container -> Int                      -- Recibe un container y devuelve su peso
+peso (Con s i) = i
+
+netS :: Stack -> Int                          -- responde el peso neto de los contenedores en la pila
+netS (Sta xs n) = sum [listaP]
+
+
+
+
+holdsS :: Stack -> Container -> Route -> Bool -- indica si la pila puede aceptar el contenedor considerando las ciudades en la ruta
+
+
+listaD :: [Container] -> [Int]                -- Recibe una lista de containers y devuelve una lista con los pesos de cada container
+listaD xs = [destino y|y<-xs]
+
+destino :: Container -> Int                      -- Recibe un container y devuelve su peso
+destino (Con s i) = s
+
+
+popS :: Stack -> String -> Stack              -- quita del tope los contenedores con destino en la ciudad indicada
+popS (Sta xs i) 
+
+
+['a','a']
+
+
+
