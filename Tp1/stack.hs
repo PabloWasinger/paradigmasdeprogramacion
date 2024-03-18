10e737cf143d7e90f3a669e69e9c9fa4f20f46cc
pablowasinger <pablowasinger@protonmail.com>
2024-03-18 15:05:23 -0300

    uyh

--- Tp1/stack.hs
+++ Tp1/stack.hs
@@ -2,7 +2,7 @@ module Stack ( Stack,newS, freeCellsS, stackS, netS, holdsS, popS )
  where
 
 --import Route
---import Container
+import Container
 
 
 data Container = Con String Int deriving (Eq, Show)
@@ -22,10 +22,8 @@ stackS (Sta xs n) (Con s i) = Sta (xs ++ [Con s i]) n
 
 
 listaP :: [Container] -> [Int]                -- Recibe una lista de containers y devuelve una lista con los pesos de cada container
-listaP xs = [peso y|y<-xs]
+listaP xs = [Container.netC y|y<-xs]
 
-peso :: Container -> Int                      -- Recibe un container y devuelve su peso
-peso (Con s i) = i
 
 netS :: Stack -> Int                          -- responde el peso neto de los contenedores en la pila
 netS (Sta xs n) = sum (listaP xs)
@@ -35,16 +33,14 @@ holdsS s c r = True -- HACER
 
 
 listaD :: [Container] -> String -> Int               -- Recibe una lista de containers y devuelve un int que refiere a la cantidad de containers a quitar
-listaD xs s = length xs - length [destino y|y<-xs, destino y == s]
-
-destino :: Container -> String                      -- Recibe un container y devuelve su destino
-destino (Con s i) = s
+listaD xs s = length xs - length [Container.destinationC y|y<-xs, destino y == s]
 
 nuevaLista :: [Container] -> String -> [Container]
 nuevaLista xs s = take (listaD xs s) xs
 
 popS :: Stack -> String -> Stack              -- quita del tope los contenedores con destino en la ciudad indicada
 popS (Sta xs i) s = (Sta (nuevaLista xs s) i)
+
 
 
 
