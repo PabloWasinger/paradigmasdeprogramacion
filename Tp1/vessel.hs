f317b4f56c00e2d3130850246329b31abe430689
pablowasinger <pablowasinger@protonmail.com>
2024-03-21 09:16:57 -0300

    fin del codigo, falta el test

--- Tp1/vessel.hs
+++ Tp1/vessel.hs
@@ -27,16 +27,17 @@ maxWeight :: Stack -> Container -> Bool                  -- Se fija si el stack 
 maxWeight stack container = (netS stack) + (netC container) < 20
 
 
-pickS :: [Stack] -> Int -> Container -> Route -> Int                       -- devuelve el indice del stack en el que pondremos el contenedor
+pickS :: [Stack] -> Int -> Container -> Route -> Int               -- devuelve el indice del stack en el que pondremos el contenedor
 pickS stacks num container route | num > length stacks - 1 = error "No hay bahías disponibles para este container"
                            |(maxWeight (stacks !! num) container) && (holdsS (stacks !! num) container route)&& (freeCellsS (stacks !! num) /= 0) = num
                            |otherwise = pickS stacks (num + 1) container route
 
 
-listaS :: [Stack] -> Int -> Container -> [Stack]           
+listaS :: [Stack] -> Int -> Container -> [Stack]       -- Crea una nueva lista de stacks, modificando el que se le carga un nuevo container      
 listaS stacks num con = take num stacks ++ [stackS (stacks !! num) con] ++ drop (num + 1) stacks
 
-
+loadV :: Vessel -> Container -> Vessel -- carga un contenedor en el barco
+loadV (Ves stacks r) container = Ves (listaS stacks (pickS stacks 0 container r) container) r
 
 unloadV :: Vessel -> String -> Vessel  -- responde un barco al que se le han descargado los contenedores que podían descargarse en la ciudad
 unloadV (Ves stacks r) ciudad = Ves [popS y ciudad|y <- stacks] r
