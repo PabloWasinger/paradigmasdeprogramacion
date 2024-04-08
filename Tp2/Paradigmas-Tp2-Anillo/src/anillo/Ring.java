9f1bb22de31d068d7a92edde276a6baa373aa5a0
pablowasinger <pablowasinger@protonmail.com>
2024-04-08 15:11:29 -0300

    das

---
+++ Tp2/Paradigmas-Tp2-Anillo/src/anillo/Ring.java
@@ -0,0 +1,25 @@
+package anillo;
+
+public class Ring {
+    public Ring next() {
+        //  complete next function
+
+
+        return null;
+    }
+
+
+    public Object current() {
+        // complete current function
+
+        return null;
+    }
+
+    public Ring add( Object cargo ) {
+        return null;
+    }
+
+    public Ring remove() {
+        return null;
+    }
+}
