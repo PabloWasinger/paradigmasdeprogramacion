235cb82e4e0aa4c8fb4925483fa0b269d45e25e3
pablowasinger <pablowasinger@protonmail.com>
2024-04-09 13:20:07 -0300

    Creación de clase Nodo y modificación de algunos métodos de Ring

--- Tp2/Paradigmas-Tp2-Anillo/src/anillo/Ring.java
+++ Tp2/Paradigmas-Tp2-Anillo/src/anillo/Ring.java
@@ -1,25 +1,64 @@
 package anillo;
 
 public class Ring {
+    public Node current_node;
+
+
     public Ring next() {
-        //  complete next function
-
-
-        return null;
+        current_node = current_node.getNext();
+        return this;
     }
 
 
     public Object current() {
-        // complete current function
-
-        return null;
+        return current_node.getValue();
     }
 
-    public Ring add( Object cargo ) {
+    public Ring add(Object cargo) {
+        // Ver como ponerle el next al nuevo nodo
+        Node new_node = new Node(cargo, null);
+
+        if (current_node == null) {
+            current_node = new_node;
+            current_node.setNext(current_node);
+            return this;
+        }
+        else{
+            current_node.setNext(new_node);
+        }
         return null;
     }
 
     public Ring remove() {
         return null;
     }
+
+
+    private static class Node {
+        private Object value;
+        private Node next;
+        private Node(Object value, Node next) {
+            this.value = value;
+            this.next = next;
+
+        }
+
+        // Setters y Getters de next y value
+        private Node getNext() {
+            return next;
+        }
+
+        private void setNext(Node next) {
+            this.next = next;
+        }
+
+        private Object getValue() {
+            return value;
+        }
+
+        private void setValue(Object value) {
+            this.value = value;
+        }
+    }
+
 }
