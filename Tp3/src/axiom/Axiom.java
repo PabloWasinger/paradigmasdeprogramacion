15a2e8152274d01e4b5bd7c7af31330125ed4bf4
pablowasinger <pablowasinger@protonmail.com>
2024-05-11 20:40:28 -0300

    tests

--- Tp3/src/axiom/Axiom.java
+++ Tp3/src/axiom/Axiom.java
@@ -3,26 +3,63 @@ package axiom;
 public class Axiom {
     private int speed = 0;
     private char heading = 'N';
+    private string probe = "not deployed"
+
 
     public int speed() {
         return speed;
     }
+
     public char heading() {
         return heading;
     }
 
+    public string probe() {
+        return probe;
+    }
+
     public Axiom process(char command) {
         if (command == 's') {
-            if (speed == 0){
-
-            }
+            this.decreaseSpeed();
+        } else
+        if (command == 'i') {
+            this.increasespeed();
         }
 
-        if(command == 'd'){
-            if (speed == 0){
-                throw new RuntimeException("too slow");
-            }
+        if (command == 'd') {
+            this.deployProbe();
         }
+        return this;
+    }
+
+
+    public Axiom increaseSpeed() {
+
+        speed += 10;
+        return this;
+    }
+
+
+    public Axiom decreaseSpeed () {
+
+        if (speed == 0) {
+            throw new RuntimeException("parado");
+        }
+        else {
+            speed -= 10;
+        }
+        return this;
+    }
+
+    public Axiom deployProbe() {
+        if (speed == 0){
+            throw new RuntimeException("too slow");
+        }
+
+        else;
+            probe = "deployed";
+        }
+
         return this;
     }
 
--- Tp3/src/axiom/AxiomTest.java
+++ Tp3/src/axiom/AxiomTest.java
@@ -8,25 +8,39 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AxiomTest {
 
-    @Test void test01() {
+    @Test void test01SpeedStartsNull() {
         assertEquals(0, newBoat().speed() );
 
     }
-    @Test void test02() {
+
+    @Test void test02IncrementsSpeedCorrectly() {assertEquals(10, newBoat().process('i').speed() );}
+    }
+
+    @Test void test03DecreasesSpeedCorrectly( ) { assertEquals(0, newBoat().process('i').speed().process('s') );}
+
+    @Test void test04CantDecreaseSpeedWhenStopped() {
+        assertThrowsLike("parado" ,() -> newBoat().process('s');
+    }
+
+    @Test void test05HeadingStartsNorh() {
         assertEquals('N' , newBoat().heading() );
     }
 
-    @Test void test03() {
-        assertEquals(0 , newBoat().process('s').speed() );
+    @Test void test06ProbeStartsNotDeployed() {
+        assertEquals("not deployed" , newBoat().probe() );
     }
 
-    @Test void test04() {
+    @Test void test07tToSlowToDeploy() {
         assertThrowsLike("too slow" ,() -> newBoat().process('d'));
     }
 
-    @Test void test05() {
+    @Test void test05RotatesLeftCorrectly() {
         assertEquals('E' , newBoat().rotate('l') );
     }
+
+
+
+
 
 
     private Axiom newBoat() {
