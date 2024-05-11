178990c0bae79770368f8520f6b46fc06789411b
pablowasinger <pablowasinger@protonmail.com>
2024-05-11 20:05:54 -0300

    asdas

--- Tp2/Paradigmas-Tp2-Anillo/src/anillo/Ring.java
+++ Tp2/Paradigmas-Tp2-Anillo/src/anillo/Ring.java
@@ -18,22 +18,17 @@ public class Ring {
     }
 
     public Ring add(Object cargo) {
-        Node new_node = new NonEmptyNode(cargo);
-        current_node = current_node.getNode(new_node); // Linea esencial, Si es un empty node, devuelve el nuevo nonemprty creado, si es un nonempty, se devuelve a el mismo
-        Node node_to_connect = current_node;
-        iterate_node(node_to_connect);
-        current_node.setNext(new_node);
-        new_node.setNext(node_to_connect);
-        current_node = new_node;
-
+        current_node = current_node.add(cargo);
         return this;
     }
 
 
     public Ring remove() {
-        Node node_to_remove = current_node;
-        iterate_node(node_to_remove);
-        current_node = node_to_remove.removeNode(current_node);
+        if (current_node == current_node.next()){
+            current_node = new EmptyNode();
+            return this;
+        }
+        current_node = current_node.remove();
         return this;
     }
 
